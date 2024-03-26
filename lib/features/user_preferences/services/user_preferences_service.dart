f30aa70c0ea4910b53d0cc1f97cf012a28597669
Sconl Peter <sconl@proton.me>
2024-03-26 04:29:20 +0300

    first attempt of the tile widget

--- lib/features/user_preferences/services/user_preferences_service.dart
+++ lib/features/user_preferences/services/user_preferences_service.dart
@@ -3,48 +3,64 @@
 // Email: sconl@proton.me
 // Description: This file defines the UserPreferencesService class.
 
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart'; // Import firebase_storage package
 import '../models/user_preferences_model.dart';
 
 class UserPreferencesService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance; // Add this line
 
   Future<void> storeUserPreferences(UserPreferences userPreferences) async {
+  String profileImageUrl = '';
+  try {
+    if (userPreferences.profileImage != null) {
+      final Reference ref = _storage.ref().child('profileImages').child('${userPreferences.username}.jpg');
+      final UploadTask uploadTask = ref.putFile(File(userPreferences.profileImage!)); // Convert String to File
+      final TaskSnapshot downloadUrl = (await uploadTask);
+      profileImageUrl = (await downloadUrl.ref.getDownloadURL());
+    }
+
     await _firestore.collection('userPreferences').doc(userPreferences.username).set({
-      'profileImage': userPreferences.profileImage,
-      'username': userPreferences.username,
-      'name': userPreferences.name,
-      'emailAddress': userPreferences.emailAddress,
-      'phoneNumber': userPreferences.phoneNumber,
-      'birthday': userPreferences.birthday,
-      'dietaryPreferences': userPreferences.dietaryPreferences,
-      'cuisinePreferences': userPreferences.cuisinePreferences,
-      'deliveryFrequency': userPreferences.deliveryFrequency,
-      'timeOfDay': userPreferences.timeOfDay,
-      'orderSize': userPreferences.orderSize,
-      'deliveryInstructions': userPreferences.deliveryInstructions,
-      'paymentMethod': userPreferences.paymentMethod,
-      'savePaymentInfo': userPreferences.savePaymentInfo,
-      'foodAllergies': userPreferences.foodAllergies,
-      'spiceLevel': userPreferences.spiceLevel,
-      'dishPreferences': userPreferences.dishPreferences,
-      'supportEcoFriendlyRestaurants': userPreferences.supportEcoFriendlyRestaurants,
-      'supportCommunityRestaurants': userPreferences.supportCommunityRestaurants,
-      'theme': userPreferences.theme,
-      'language': userPreferences.language,
-      'pushNotifications': userPreferences.pushNotifications,
-      'emailNotifications': userPreferences.emailNotifications,
-      'orderHistoryVisibility': userPreferences.orderHistoryVisibility,
-      'socialMediaIntegration': userPreferences.socialMediaIntegration,
-      'marketingPreferences': userPreferences.marketingPreferences,
-      'appVersion': userPreferences.appVersion,
-      'privacySettings': userPreferences.privacySettings,
-      'portionSize': userPreferences.portionSize, // New field
-      'studentDeals': userPreferences.studentDeals, // New field
-      'supportLocal': userPreferences.supportLocal, // New field
-      'lateNightDelivery': userPreferences.lateNightDelivery, // New field
-      'affordabilityFilters': userPreferences.affordabilityFilters, // New field
-      'campusPartnerships': userPreferences.campusPartnerships, // New field
-    });
+      'profileImage': profileImageUrl, // Store the URL of the uploaded image
+        'username': userPreferences.username,
+        'name': userPreferences.name,
+        'emailAddress': userPreferences.emailAddress,
+        'phoneNumber': userPreferences.phoneNumber,
+        'birthday': userPreferences.birthday,
+        'dietaryPreferences': userPreferences.dietaryPreferences,
+        'cuisinePreferences': userPreferences.cuisinePreferences,
+        'deliveryFrequency': userPreferences.deliveryFrequency,
+        'timeOfDay': userPreferences.timeOfDay,
+        'orderSize': userPreferences.orderSize,
+        'deliveryInstructions': userPreferences.deliveryInstructions,
+        'paymentMethod': userPreferences.paymentMethod,
+        'savePaymentInfo': userPreferences.savePaymentInfo,
+        'foodAllergies': userPreferences.foodAllergies,
+        'spiceLevel': userPreferences.spiceLevel,
+        'dishPreferences': userPreferences.dishPreferences,
+        'supportEcoFriendlyRestaurants': userPreferences.supportEcoFriendlyRestaurants,
+        'supportCommunityRestaurants': userPreferences.supportCommunityRestaurants,
+        'theme': userPreferences.theme,
+        'language': userPreferences.language,
+        'pushNotifications': userPreferences.pushNotifications,
+        'emailNotifications': userPreferences.emailNotifications,
+        'orderHistoryVisibility': userPreferences.orderHistoryVisibility,
+        'socialMediaIntegration': userPreferences.socialMediaIntegration,
+        'marketingPreferences': userPreferences.marketingPreferences,
+        'appVersion': userPreferences.appVersion,
+        'privacySettings': userPreferences.privacySettings,
+        'portionSize': userPreferences.portionSize, // New field
+        'studentDeals': userPreferences.studentDeals, // New field
+        'supportLocal': userPreferences.supportLocal, // New field
+        'lateNightDelivery': userPreferences.lateNightDelivery, // New field
+        'affordabilityFilters': userPreferences.affordabilityFilters, // New field
+        'campusPartnerships': userPreferences.campusPartnerships, // New field
+      });
+    } catch (e) {
+      print('Error occurred while storing user preferences: $e');
+      throw e;
+    }
   }
 }
--- lib/features/user_preferences/widgets/onboarding_welcome_details.dart
+++ lib/features/user_preferences/widgets/onboarding_welcome_details.dart
@@ -3,29 +3,113 @@
 // Email: sconl@proton.me
 // Description: This file contains the OnboardingWelcomeDetails widget.
 
+import 'dart:io'; // Import dart:io to use the File class
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart'; // Import image_picker package
 import '../../../shared/theme/custom_theme.dart'; // Import custom theme
 
-class OnboardingWelcomeDetails extends StatelessWidget {
+class OnboardingWelcomeDetails extends StatefulWidget {
   final List<String> selectedPreferences;
   final Function(String) togglePreference;
 
   OnboardingWelcomeDetails(this.selectedPreferences, this.togglePreference);
 
   @override
+  _OnboardingWelcomeDetailsState createState() => _OnboardingWelcomeDetailsState();
+}
+
+class _OnboardingWelcomeDetailsState extends State<OnboardingWelcomeDetails> {
+  late TextEditingController _usernameController;
+  late TextEditingController _phoneNumberController;
+  late DateTime _selectedDate;
+  late File _imageFile; // Change this to File
+
+  @override
+  void initState() {
+    super.initState();
+    _usernameController = TextEditingController();
+    _phoneNumberController = TextEditingController();
+    _selectedDate = DateTime.now();
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _phoneNumberController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _pickDate() async {
+    try {
+      final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: _selectedDate,
+        firstDate: DateTime(1900),
+        lastDate: DateTime.now(),
+      );
+      if (picked != null && picked != _selectedDate) {
+        setState(() {
+          _selectedDate = picked;
+        });
+      }
+    } catch (e) {
+      print('Error occurred while picking date: $e');
+    }
+  }
+
+  Future<void> _pickImage() async {
+    try {
+      final ImagePicker _picker = ImagePicker();
+      final XFile? pickedFile = await _picker.pickImage(source: ImageSource.gallery); // Use pickImage instead of getImage
+      if (pickedFile != null) {
+        setState(() {
+          _imageFile = File(pickedFile.path); // Convert XFile to File
+        });
+      }
+    } catch (e) {
+      print('Error occurred while picking image: $e');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          'Account & Profile',
-          style: MyTheme.lightTheme.textTheme.headlineLarge,
-        ),
-        Text(
-          'Profile Image: Allow users to upload a profile picture.',
-          style: MyTheme.lightTheme.textTheme.bodyLarge,
-        ),
-        // Add other fields here...
-      ],
+    return Container(
+      color: Theme.of(context).primaryColor.withOpacity(0.5), // Background color with 50% opacity
+      child: Column(
+        children: [
+          Text(
+            'Account & Profile',
+            style: MyTheme.lightTheme.textTheme.headlineLarge,
+          ),
+          TextButton(
+            onPressed: _pickImage,
+            child: Text('Upload Profile Image'),
+          ),
+          TextField(
+            controller: _usernameController,
+            decoration: InputDecoration(
+              labelText: 'Username',
+            ),
+          ),
+          TextField(
+            controller: _phoneNumberController,
+            decoration: InputDecoration(
+              labelText: 'Phone Number',
+            ),
+          ),
+          TextButton(
+            onPressed: _pickDate,
+            child: Text('Select Birthday'),
+          ),
+          TextButton(
+            onPressed: () {
+              // Call your method to store preferences here
+              // You will need to pass the username, phone number, birthday, and image file
+            },
+            child: Text('Next'),
+          ),
+        ],
+      ),
     );
   }
 }
